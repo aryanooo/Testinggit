8cb5addf6b21ad427322d24ae3674d8b8a162998
Aniket Chourasiya <131843728+Aniket1603@users.noreply.github.com>
2024-12-19 09:56:03 +0530

    first commit

---
+++ Testinggit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Testinggit {
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two number");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a+b);
+
+    }
+
+
+
+
+}
